91533a8b17e8e8d764d78196f5545c4865955847
kingputer <suckedmeinahondacivic@gmail.com>
2023-11-03 11:18:01 -0700

    new routing methods .. now with functionality

--- routes/index.js
+++ routes/index.js
@@ -10,3 +10,28 @@ const router = express.Router();
 const CatalogList = require('../models/catalog-list');
 const LocationInfo = require('../models/location-info');
 
+// router.use('/checkout', require('./checkout'));
+// router.use('/order-confirmation', require('./order-confirmation'));
+
+router.get('/', async (req, res, next) => {
+    const types = "ITEM,IMAGE"; // retrieve ITEM & IMAGE CatalogObjects
+    try {
+        const { result: { locations } } = await locationsApi.listLocations(); // retrieve locations
+        const { result: { objects } } = await catalogApi.listCatalog(undefined, types); // retrieve catalogItem & catalogImage
+        
+        res.render('index', {  
+            /**
+             * todo: implement when frontend exists lol
+             * 
+             * items: new CatalogList(objects).items,
+             * locationInfo: new LocationInfo(locations[0]),
+             * 
+            */
+        })
+
+    } catch (err) {
+        next(err);
+    }
+})
+
+module.exports = router;
