7296101f5f337921909bed78d0ff94e4506b7bad
heisenhook <suckedmeinahondacivic@gmail.com>
2023-11-20 14:19:20 -0800

    api for catalog and locations

--- routes/index.js
+++ routes/index.js
@@ -30,4 +30,27 @@ router.get('/', async (req, res, next) => {
     }
 })
 
+router.get('/api/catalog/', async (req, res, next) => {
+   const types = "ITEM,IMAGE";
+   try {
+    const { result: { objects } } = await catalogApi.listCatalog(undefined, types);
+
+    res.json(objects);
+    
+    } catch (err) {
+       next(err);
+    }
+});
+
+router.get('/api/locations/', async (req, res, next) => {
+    try {
+     const { result: { locations } } = await locationsApi.listLocations();
+
+     res.json(locations);
+     
+     } catch (err) {
+        next(err);
+     }
+ }); 
+
 module.exports = router;
