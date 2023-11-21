900e272aff8a2c721f37b9bcd92fe972b12c0aed
heisenhook <suckedmeinahondacivic@gmail.com>
2023-11-20 18:50:17 -0800

    fetch call catalog items

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,14 @@
 const socket = io('ws://localhost:8080');
 
-var catalogItems = [];
 var cartItems = [];
+var catalogItems = fetch('http://localhost:3000/api/catalog')
+    .then(response => response.json())  // Corrected this line
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('failed to fetch catalog items: ', error);
+});
 
 function init() {
     catalogItems = document.getElementsByClassName('catalogItem');
