677d19d8fe206434f05aa44f50170f74768de69d
heisenhook <suckedmeinahondacivic@gmail.com>
2023-11-27 16:46:15 -0800

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -126,7 +126,7 @@ function buildCart(id = -1) {
 }
 
 function init() {
-    fetch(`${window.location}/api/catalog`)
+    fetch(`${window.location.origin}/api/catalog`)
         .then(response => response.json())
         .then(data => {
             square = data;
