4166e74cdd0685af86ca3c5b36582470aa3b68fb
heisenhook <suckedmeinahondacivic@gmail.com>
2023-11-28 08:46:07 -0800

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 let hostname = 'localhost';
 if (window.location.hostname == 'learncpp.today') {
-    hostname == 'demo.learncpp.today';
+    hostname == '66.94.112.161';
 } // can't think of any better solution, this might not even work!
 
 const socket = io(`ws://${hostname}:8080`);
