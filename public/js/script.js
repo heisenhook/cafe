8ab29877bfa2e2efec5bc9a34f56701faa89b21e
heisenhook <suckedmeinahondacivic@gmail.com>
2023-11-28 19:17:36 -0800

    testing

--- public/js/script.js
+++ public/js/script.js
@@ -2,8 +2,6 @@ let hostname = 'localhost';
 if (window.location.hostname == 'learncpp.today') {
     hostname == '66.94.112.161';
 } // can't think of any better solution, this might not even work!
-
-const socket = io(`ws://${hostname}:8080`);
 var square = [];
 var catalog = [];
 var cart = {
