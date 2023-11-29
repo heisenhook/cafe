d8d65f196b06002bd4fa0714857245944ec5786d
heisenhook <suckedmeinahondacivic@gmail.com>
2023-11-28 19:18:47 -0800

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,7 @@ let hostname = 'localhost';
 if (window.location.hostname == 'learncpp.today') {
     hostname == '66.94.112.161';
 } // can't think of any better solution, this might not even work!
+const socket = io(`ws://demo.learncpp.today`);
 var square = [];
 var catalog = [];
 var cart = {
